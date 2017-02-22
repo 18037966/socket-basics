941fe85ed8bc2c002fa4b5d1a3becf44f7830b9f
Zubaiyer Karim Labib <zbaiyer.labib@gmail.com>
2017-02-22 17:23:09 +1100

    Show timestamp on UI Lecture94

--- public/js/app.js
+++ public/js/app.js
@@ -5,10 +5,11 @@ socket.on('connect', function () {
 });
 
 socket.on('message', function (message) {
+	var momentTimestamp = moment.utc(message.timestamp);
 	console.log('New message:');
 	console.log(message.text);
 	
-	jQuery('.messages').append('<p>' + message.text + '</p>');
+	jQuery('.messages').append('<p><strong>' + momentTimestamp.local().format('h:mm a') + ': </strong>'  + message.text + '</p>');
 });
 
 var $form = jQuery('#message-form');
@@ -17,10 +18,12 @@ $form.on('submit', function (event) {
 	event.preventDefault();
 
 	var $message = $form.find('input[name=message]');
-
+    
 	socket.emit('message', {
 		text: $message.val()
 	});
 
 	$message.val('');
 });
+
+
